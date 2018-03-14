22d6a720961f0f26b48d61e7c7db05cf2b4983c8
Mário Antunes <mariolpantunes@gmail.com>
2018-03-14 15:34:39 +0000

    Using the find closer element from ArrayUtils.

--- src/main/java/pt/it/av/atnog/ml/clustering/curvature/Rmethod.java
+++ src/main/java/pt/it/av/atnog/ml/clustering/curvature/Rmethod.java
@@ -25,7 +25,7 @@ public class Rmethod implements Curvature {
 
     //System.out.println("R2="+r2);
     double a = lnr[0];
-    System.out.println("A=" + a);
+    //System.out.println("A=" + a);
 
     double rx = x[0];
 
@@ -40,20 +40,9 @@ public class Rmethod implements Curvature {
 
     // Ln alternative close formula
     rx = a / Math.sqrt(2.0);
-    System.out.println("FLN(" + rx + ") = " + flnr(rx, a));
+    //System.out.println("FLN(" + rx + ") = " + flnr(rx, a));
 
-    int idx = 0;
-    double err = Math.abs(x[0] - rx);
-
-    for (int i = 1; i < x.length; i++) {
-      double t = Math.abs(x[i] - rx);
-      if (t < err) {
-        idx = i;
-        err = t;
-      }
-    }
-
-    return idx;
+    return ArrayUtils.findCloseSorted(rx, x);
   }
 
   private double flnr(double rx, double a) {
@@ -76,19 +65,18 @@ public class Rmethod implements Curvature {
     }
 
     double r2 = ArrayUtils.r2(y, f);
-
     //System.out.println("R2="+r2);
 
     double a = pr[0], b = pr[1];
     double u = a * b, v = u * (b - 1), k = v * (b - 2);
     double z = k * u * u - 3 * v * v * u;
 
-    System.out.println("A=" + a);
-    System.out.println("B=" + b);
-    System.out.println("U=" + u);
-    System.out.println("V=" + v);
-    System.out.println("K=" + k);
-    System.out.println("Z=" + z);
+    //System.out.println("A=" + a);
+    //System.out.println("B=" + b);
+    //System.out.println("U=" + u);
+    //System.out.println("V=" + v);
+    //System.out.println("K=" + k);
+    //System.out.println("Z=" + z);
 
     double rx = x[0];
 
@@ -103,21 +91,9 @@ public class Rmethod implements Curvature {
 
     // Ln alternative
     rx = Math.pow((k / -z), (1.0 / (2 * b - 2)));
+    //System.out.println("FLN(" + rx + ") = " + fpr(rx, k, z, b));
 
-    System.out.println("Fa1(" + rx + ") = " + fpr(rx, k, z, b));
-
-    int idx = 0;
-    double err = Math.abs(x[0] - rx);
-
-    for (int i = 1; i < x.length; i++) {
-      double t = Math.abs(x[i] - rx);
-      if (t < err) {
-        idx = i;
-        err = t;
-      }
-    }
-
-    return idx;
+    return ArrayUtils.findCloseSorted(rx, x);
   }
 
   private double fpr(double x, double k, double z, double b) {
